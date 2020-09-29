7230ea9a9f029921c68896f54148a8a6097c30eb
hariprakash <harip7873@gmail.com>
2020-09-29 18:47:44 +0530

    Web Driver Added

--- speech_recog.py
+++ speech_recog.py
@@ -1,24 +1,29 @@
 import speech_recognition as sr
 import pyttsx3 as p
 from selenium import webdriver
+class Raven():
+    def __init__(self):
+        self.driver=webdriver.Ie(executable_path='C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe')
 
 
-def speak(ip):
-    e=p.init()
-    e.say(ip)
-    e.runAndWait()
 
-def recogonise():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("speak")
-        input = r.listen(source)
-        try:
-            processed_text = r.recognize_google(input)
-            return processed_text
-        except sr.UnknownValueError:
-            return "Raven cannot recognise your voice"
+    def speak(self,ip):
+        e=p.init()
+        e.say(ip)
+        e.runAndWait()
 
-x=recogonise()
+    def recogonise(self):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("speak")
+            input = r.listen(source)
+            try:
+                processed_text = r.recognize_google(input)
+                return processed_text
+            except sr.UnknownValueError:
+                return "Raven cannot recognise your voice"
+
+one=Raven()
+x=one.recogonise()
 print(x)
-speak(x)
+one.speak(x)
