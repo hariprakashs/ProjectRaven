f60ec544864336ebc530d416b5ed67144a95d1dd
hariprakash <harip7873@gmail.com>
2020-09-29 18:11:34 +0530

    Functions Created

--- speech_recog.py
+++ speech_recog.py
@@ -1,11 +1,24 @@
 import speech_recognition as sr
-r = sr.Recognizer()
+import pyttsx3 as p
+from selenium import webdriver
 
-with sr.Microphone() as source:
-    input = r.listen(source)
 
-    try:
-        processed_text = r.recognize_google(input)
-        print(processed_text)
-    except sr.UnknownValueError:
-        print("Google cannot recognise your voice one")
+def speak(ip):
+    e=p.init()
+    e.say(ip)
+    e.runAndWait()
+
+def recogonise():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("speak")
+        input = r.listen(source)
+        try:
+            processed_text = r.recognize_google(input)
+            return processed_text
+        except sr.UnknownValueError:
+            return "Raven cannot recognise your voice"
+
+x=recogonise()
+print(x)
+speak(x)
